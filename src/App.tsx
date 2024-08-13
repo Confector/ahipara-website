c21a4815e38db8017ca1e495eee36d15d06ffbcb
Thomas Reinsch <thomasreinsch@Confectors-MacBook.local>
2024-08-13 20:02:35 +0300

    websiteAvailable status implemented.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './styles/globals.css';
-// import Start from './pages/Start/Start';
+import Start from './pages/Start/Start';
 
 const App = () => {
+  const websiteIsAvailable = false;
   return (
     <div>
-      <h1>
-        <center>ahipara.de</center>
-      </h1>
+      {websiteIsAvailable ? (
+        <Start />
+      ) : (
+        <div>
+          <h1>
+            <center>ahipara.de</center>
+          </h1>
 
-      <center>
-        We're under construction. Please check back for an update soon.
-      </center>
+          <center>
+            We're under construction. Please check back for an update soon.
+          </center>
+        </div>
+      )}
     </div>
-    // <Start />
   );
 };
 
