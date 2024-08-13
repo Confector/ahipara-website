00eb34d9821d58a1cb950029e20d45a4ca4bd904
Thomas Reinsch <thomasreinsch@Confectors-MacBook.local>
2024-08-13 16:52:33 +0300

    test index changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './styles/globals.css';
 
 const App = () => {
   return (
-    // <Start />
     <div>
       <h1>
         <center>ahipara.de</center>
@@ -14,6 +13,7 @@ const App = () => {
         We're under construction. Please check back for an update soon.
       </center>
     </div>
+    // <Start />
   );
 };
 
