ec09d3ce1588d93a7c7d9520b6d8fdc1e70b6efd
Thomas Reinsch <thomas@reinsch.org>
2024-08-14 09:07:31 +0300

    status page modified.

--- src/pages/Status/WebsiteNotAvailable.tsx
+++ src/pages/Status/WebsiteNotAvailable.tsx
@@ -4,17 +4,19 @@ import React from 'react';
 const websiteIsNotAvailable = () => {
   return (
     <div className='content-center text-center h-screen'>
-      <div className='flex justify-center'>
-        <div style={{ width: '152px' }}>
-          <Logo />
+      <div>
+        <div className='flex justify-center'>
+          <div style={{ width: '152px' }}>
+            <Logo />
+          </div>
         </div>
+        <h2 className='font-bold m-4'>Wir gestalten gerade um.</h2>
+        <p style={{ lineHeight: 1.5 }}>
+          Bitte schauen Sie bald wieder vorbei.
+          <br />
+          Vielen Dank für Ihr Verständnis!
+        </p>
       </div>
-      <h2 className='font-bold m-4'>Wir gestalten gerade um.</h2>
-      <p style={{ lineHeight: 1.5 }}>
-        Bitte schauen Sie bald wieder vorbei.
-        <br />
-        Vielen Dank für Ihr Verständnis!
-      </p>
     </div>
   );
 };
